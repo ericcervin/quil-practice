50e70d3c1137904e06dbcc6a7bc1a996e6c30f29
Eric Ervin <ericcervin@gmail.com>
2019-10-29 20:46:09 -0400

    added quilpractice20190923.clj

---
+++ src/quil_practice/quilpractice20190923.clj
@@ -0,0 +1,42 @@
+(ns quil-practice.quilpractice20190923
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+
+(defn pxl-rgb [n pxls]
+  (let [p (aget pxls n)
+        r (q/red p)
+        g (q/green p)
+        b (q/blue p)]
+    [r g b]))
+
+(defn setup []
+  (q/frame-rate 12)
+  (q/blend-mode :difference)
+  (let [img  (q/load-image "/resources/img-a.jpg")        
+        pxls  (q/pixels img)]
+    {:pxls pxls}))
+       
+
+(defn update-state [state]
+   state) 
+
+(defn draw [state]
+  (let [pxls (:pxls state)]
+    (doseq [i (range 10)]
+      (let [xy (* i 25)
+            hw (- 500 (* 2 xy))]
+        (apply q/fill (pxl-rgb (+ (q/frame-count) i) pxls))
+        (q/rect xy xy hw hw))))  
+  
+  #_(if (< (q/frame-count) 720) 
+     (do (println (q/frame-count))
+       (q/save-frame "/target/output-####.png"))))    
+
+(q/defsketch nested-pxl-squares
+  :size [500 500]
+  :setup setup
+  :draw draw
+  :update update-state
+  :features [:keep-on-top]
+  :middleware [m/fun-mode])
